8831dc552d1604fa7356b5b4df20285b607b999f
danse <f.occhipinti@gmail.com>
2014-04-15 13:58:58 +0000

    add models for twitter ingestion configuration

---
+++ api/models/Twt_oauths.js
@@ -0,0 +1,19 @@
+/**
+ * Twt_oauths
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	
+  	/* e.g.
+  	nickname: 'string'
+  	*/
+    
+  }
+
+};
