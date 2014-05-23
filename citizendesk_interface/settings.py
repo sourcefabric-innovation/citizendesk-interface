025dda053df9b6d834b8ccb89e6165226f97abf8
danse <f.occhipinti@gmail.com>
2014-05-23 17:39:08 +0000

    enable cross origin requests

--- citizendesk_interface/settings.py
+++ citizendesk_interface/settings.py
@@ -2,6 +2,8 @@ from .entities.reports import entity as reports
 
 settings = {
     'MONGO_DBNAME': 'citizendesk',
+    'X_DOMAINS': "*",
+    'X_HEADERS': "Content-Type",
 
     'DOMAIN': {
         'reports': reports,
