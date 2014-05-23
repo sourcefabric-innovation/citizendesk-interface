c387c6859a23bdb3b70d167c93d42e10183f8e7f
danse <f.occhipinti@gmail.com>
2014-05-23 10:44:33 +0000

    remove server name

--- citizendesk_interface/settings.py
+++ citizendesk_interface/settings.py
@@ -3,7 +3,6 @@ from entities.reports import entity as reports
 settings = {
     'MONGO_DBNAME': 'citizendesk',
 
-    'SERVER_NAME': '127.0.0.1:5000',
     'DOMAIN': {
         'reports': reports,
     }
