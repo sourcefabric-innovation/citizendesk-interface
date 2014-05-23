a71e08b93a9e9b7f7d20988878a4e1709dd7bdff
danse <f.occhipinti@gmail.com>
2014-05-23 17:39:35 +0000

    add read only support for most of the existing entities

--- citizendesk_interface/settings.py
+++ citizendesk_interface/settings.py
@@ -7,5 +7,12 @@ settings = {
 
     'DOMAIN': {
         'reports': reports,
+        'twt_streams': {},
+        'twt_filters': {},
+        'twt_oauths': {},
+        'steps': {},
+        'settings-bool': {},
+        'settings-int': {},
+        'settings-string': {}
     }
 }
