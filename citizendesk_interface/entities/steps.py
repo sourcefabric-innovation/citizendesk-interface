35999924b26c5bb34ff90c4ce8b12fdbce74676b
danse <francesco.occhipinti@sourcefabric.org>
2014-06-02 14:06:26 +0000

    add steps and waste bandwidth

---
+++ citizendesk_interface/entities/steps.py
@@ -0,0 +1,11 @@
+schema = {
+    'description': {
+        'type': 'string',
+    }
+}
+entity = {
+    'schema': schema,
+    'resource_methods': ['GET', 'POST'],
+    'item_methods': ['GET', 'PUT', 'PATCH'],
+    'pagination': False
+}
--- citizendesk_interface/settings.py
+++ citizendesk_interface/settings.py
@@ -1,11 +1,13 @@
 from .entities.reports import entity as reports
 from .entities.twt_searches import entity as twt_searches
+from .entities.steps import entity as steps
 
 settings = {
     'MONGO_DBNAME': 'citizendesk',
     'X_DOMAINS': "*",
     'X_HEADERS': "Content-Type",
     'XML': False,
+    'BANDWIDTH_SAVER': False,
 
     'DOMAIN': {
         'reports': reports,
@@ -13,7 +15,7 @@ settings = {
         'twt_filters': {},
         'twt_oauths': {},
         'twt-searches': twt_searches,
-        'steps': {},
+        'steps': steps,
         'settings-bool': {},
         'settings-int': {},
         'settings-string': {}
