66d87b6846ba907a85e51b932f12ce7d74339ffe
danse <francesco.occhipinti@sourcefabric.org>
2014-06-19 08:46:43 +0000

    start adding superdesk logic

--- citizendesk_interface/__init__.py
+++ citizendesk_interface/__init__.py
@@ -2,6 +2,10 @@ import logging
 
 from eve import Eve
 from raven.handlers.logging import SentryHandler
+import superdesk.users as superdesk_users
+import superdesk.auth as superdesk_auth
+from eve_docs import eve_docs
+from flask.ext.bootstrap import Bootstrap
 
 from .settings import settings as default_settings
 from .blueprints.proxy import blueprint as proxy_blueprint
@@ -12,6 +16,10 @@ handler = SentryHandler('http://b1901abf077d476ba253bce45dd5bf91:cf99fe3dade94a5
 
 def register_blueprints(app):
     app.register_blueprint(proxy_blueprint, url_prefix='/proxy')
+    superdesk_users.init_app(app)
+    superdesk_auth.init_app(app)
+    Bootstrap(app) # required by eve docs
+    app.register_blueprint(eve_docs, url_prefix='/docs')
 
 def get_app(customised={}):
     settings = default_settings.copy()
