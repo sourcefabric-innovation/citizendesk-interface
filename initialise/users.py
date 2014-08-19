15d635059816b8f46f9515c4621926161c01c882
danse <f.occhipinti@gmail.com>
2014-08-19 18:01:05 +0200

    add more test users

--- initialise/users.py
+++ initialise/users.py
@@ -4,26 +4,32 @@ from main import Init, init_collection
 
 documents = [{
         'username': 'Doug',
-        'password': 'no',
 }, {
         'username': 'Francesco',
-        'password': 'no',
 }, {
         'username': 'Martin',
-        'password': 'no',
 }, {
         'username': 'Darko',
-        'password': 'no',
 }, {
         'username': 'Erik',
-        'password': 'no',
 }, {
         'username': 'Aderito',
-        'password': 'no',
+}, {
+        'username': 'User1',
+}, {
+        'username': 'User2',
+}, {
+        'username': 'User3',
+}, {
+        'username': 'User4',
+}, {
+        'username': 'User5',
 }]
 
 # like in https://github.com/superdesk/Live-Blog/blob/63c713a90b8e464aff62a032cb5d85f6b1220d1d/plugins/livedesk-sync/livedesk/core/impl/chained_sync.py#L303
-for document in documents: document['uuid'] = str(uuid4().hex)
+for document in documents:
+    document['password'] = 'no'
+    document['uuid']     = str(uuid4().hex)
 
 init_users = Init('users', documents)
 
