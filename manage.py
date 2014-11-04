c93a988b24a46b6ca18766a8e049ecca1d932b27
danse <f.occhipinti@gmail.com>
2014-11-04 17:55:54 +0100

    try to add the hash user passwords command

--- manage.py
+++ manage.py
@@ -1,9 +1,13 @@
 from flask.ext.script import Manager
 from apps.auth.db.commands import CreateUserCommand
+from apps.auth.db.commands import HashUserPasswordsCommand
 
 from citizendesk_interface import app
 
 manager = Manager(app)
 
 if __name__ == "__main__":
-    manager.run({'create-user': CreateUserCommand()})
+    manager.run({
+        'create-user': CreateUserCommand(),
+        'hash-user-passwords': HashUserPasswordsCommand()
+    });
